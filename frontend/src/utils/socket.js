231a2099666f811efbede1fc27ad9d0c4122f010
arpitcodebuzz <arpit.codebuzz@gmail.com>
2026-03-03 15:16:36 +0530

    fronted removed from backend

--- frontend/src/utils/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-import { API } from "./api.js";
-
-export function createSocket() {
-  return io(API, {
-    withCredentials: true,
-    transports: ["websocket", "polling"], // allow fallback
-  });
-}
